2fa581a2a4d25fd665300a41088f185d326eeb98
EvsArt <x.evseev@yandex.ru>
2024-01-10 00:34:14 +0300

    added LoginServer and ProxyServer for mutiserving + added start.sh files

---
+++ start.sh
@@ -0,0 +1,12 @@
+screen -dmS proxy
+screen -dmS main
+screen -dmS login
+
+screen -S proxy -p 0 -X stuff 'cd ./ProxyServer && ./start.sh
+'
+
+screen -S main -p 0 -X stuff 'cd ./MainServer && ./start.sh
+'
+
+screen -S login -p 0 -X stuff 'cd ./LoginServer && ./start.sh
+'
